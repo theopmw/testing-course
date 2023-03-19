5ce28812c3cd833e9391c894efffcd89310424df
theopmw <theowrightdev@gmail.com>
2023-03-19 11:04:01 +0000

    Write test for transformToNumber function with multiple assertions

--- basic-testing-frontend/src/util/number.spec.js
+++ basic-testing-frontend/src/util/number.spec.js
@@ -2,18 +2,29 @@ import { it, expect } from 'vitest';
 
 import { transformToNumber } from './numbers';
 
+// it('should transform a string number into a number of type number', () => {
+//   const input = '1';
+
+//   const result = transformToNumber(input);
+
+//   expect(result).toBeTypeOf('number');
+// });
+
 it('should transform a string number into a number of type number', () => {
   const input = '1';
 
   const result = transformToNumber(input);
 
-  expect(result).toBeTypeOf('number');
+  expect(result).toBe(+input);
 });
 
 it('should yeild NaN for non-transformable values', () => {
   const input = 'invalid';
+  const input2 = {};
 
   const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
 
   expect(result).toBeNaN();
+  expect(result2).toBeNaN();
 });
