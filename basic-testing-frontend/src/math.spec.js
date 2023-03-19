f15a6ee2d10bb0ed5604a6659947e19cc5123246
theopmw <theowrightdev@gmail.com>
2023-03-19 09:56:44 +0000

    Add tests and fix add function

--- basic-testing-frontend/src/math.spec.js
+++ basic-testing-frontend/src/math.spec.js
@@ -16,3 +16,29 @@ it('should summarize all number values in an array', () => {
   // Assert
   expect(result).toBe(expectedResult);
 });
+
+it('should yeild NaN if at least one invalid number is provided', () => {
+  // Arrange
+  const inputs = ['invalid', 1];
+
+  // Act
+  const result = add(inputs);
+
+  // Assert
+  expect(result).toBeNaN();
+});
+
+it('should yeild a correct sum if an array of numeric string values is provided', () => {
+  // Arrange
+  const numbers = ['1', '2'];
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBe(expectedResult);
+});
